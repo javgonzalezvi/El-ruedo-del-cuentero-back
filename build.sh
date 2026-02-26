10e3dc8911e87999269140f4580aa476d6ca0795
Javier González <92602146+javgonzalezvi@users.noreply.github.com>
2026-02-25 23:27:05 -0500

    Actualizacion build

--- build.sh
+++ build.sh
@@ -7,3 +7,19 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear superusuario si no existe (usa variables de entorno de Render)
+python manage.py shell -c "
+from usuarios.models import Usuario
+import os
+
+correo    = os.environ.get('javgonzalezvi@unal.edu.co')
+password  = os.environ.get('Leviatan.2609*')
+nombres   = os.environ.get('Javier Esteban', 'Admin')
+apellidos = os.environ.get('Gonzalez Vivas', 'Ruedo')
+if correo and password and not Usuario.objects.filter(correo=correo).exists():
+    Usuario.objects.create_superuser(correo=correo, password=password, nombres=nombres, apellidos=apellidos)
+    print('Superusuario creado:', correo)
+else:
+    print('Superusuario ya existe o variables no definidas.')
+"
